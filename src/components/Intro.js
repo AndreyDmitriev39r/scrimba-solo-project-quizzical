3524f45e47174a3e975ba1a04e84bd9a4531fb58
Andrey Dmitriev <admit8490@gmail.com>
2023-03-08 01:01:37 +0200

    intro-styling minor fix

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -10,7 +10,7 @@ function Intro({handleStartQuizClick}) {
     intro: "h-full w-full flex flex-col justify-center items-center bg-white",
     intro_heading: "mb-2 text-4xl font-bold font-mono text-slate-700",
     intro_subheading: "mb-7 text-base font-normal font-mono text-slate-700",
-    intro_startbtn: "h-16 w-48 bg-cyan-500 rounded-2xl font-normal text-zinc-50",
+    intro_startbtn: "h-16 w-48 bg-cyan-500 rounded-2xl font-medium text-zinc-50",
   }  
 
   return (
