eabddbfa684eea760c0b369e609c64db4f9fdcfb
Andrey Dmitriev <admit8490@gmail.com>
2023-03-07 18:17:01 +0200

    start-toanswers switcher

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+import { useState } from "react";
+
 import Intro from "./components/Intro";
 import Questions from "./components/Questions";
 import Answers from "./components/Answers";
 
 function App() {
+
+  // state
+
+  const [showPage, setShowPage] = useState(false);
+
+  const handleStartQuizClick = () => {
+    setShowPage((prevShowPage) => !prevShowPage);
+  }
+
   return (
     <div className="app">
-      <Intro />
-      <Questions />
-      <Answers />
+      {!showPage &&
+      <Intro
+        handleStartQuizClick={handleStartQuizClick}
+      />}
+      {showPage && <Questions />}
+      {false && <Answers />}
     </div>
   );
 }
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,9 +1,11 @@
-function Intro() {
+function Intro({handleStartQuizClick}) {
   return (
     <div className="intro bg-green-300">
       <h1>Quizzical</h1>
       <p>Description</p>
-      <button>Start quiz</button>
+      <button
+        onClick={handleStartQuizClick}
+      >Start quiz</button>
     </div>
   );
 }
