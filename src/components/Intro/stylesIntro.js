f733d542df9c7ccaab31653d25b7c185267969e3
Andrey Dmitriev <admit8490@gmail.com>
2023-03-10 19:27:48 +0200

    added text-shadows

--- src/components/Intro/stylesIntro.js
+++ src/components/Intro/stylesIntro.js
@@ -1,6 +1,6 @@
 const stylesIntro = {
   intro: "h-full w-full flex flex-col justify-center items-center bg-white",
-  intro_heading: "mb-2 text-4xl font-bold font-karla text-slate-700",
+  intro_heading: "mb-2 text-4xl font-bold font-karla text-slate-700 drop-shadow-[1px_1px_0_gray]",
   intro_subheading: "mb-7 text-base font-normal font-inter text-slate-700",
   intro_startbtn: "h-16 w-48 bg-cyan-500 rounded-2xl font-medium font-inter text-zinc-50 shadow-md shadow-cyan-800 active:shadow-inner hover:bg-cyan-700 hover:font-semibold",
 };
--- src/components/Questions/questions-children/Question/stylesQuestion.js
+++ src/components/Questions/questions-children/Question/stylesQuestion.js
@@ -1,6 +1,6 @@
 const stylesQuestion = {
   question: "mx-14 flex flex-col gap-3",
-  question_heading: "text-base font-karla font-bold text-slate-700",
+  question_heading: "text-base font-karla font-bold text-slate-700 drop-shadow-[0.8px_0.8px_0_gray]",
   question_options_wrapper: "flex flex-row flex-wrap gap-3",
   divider: "border border-gray-300",
 };
